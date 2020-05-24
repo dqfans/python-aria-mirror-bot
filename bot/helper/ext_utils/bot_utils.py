f9c47afe1b6a4240f496595dd439a44be7d024b0
noupus <63485872+noupus@users.noreply.github.com>
2020-05-24 20:34:12 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -89,16 +89,16 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>📂 Name:</b> <i>{download.name()}</i> - "
-            msg += download.status()
+            msg += f"<b>📂 Name:</b> <i>{download.name()}</i>"
+            msg += f"\n<b>Status:</b> <code>{download.status()}</code>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
-            msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> " 
-                    msg += f"Size: {download.size()}"
-                    msg += f"Speed: {download.speed()}, ETA: {download.eta()} "
+                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> " 
+                    f"\n<b>Size:</b> {download.size()}"
+                    f"\n<b>Speed:</b> {download.speed()}\n<b>ETA:</b> {download.eta()} "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"| P: {download.aria_download().connections} " \
-                           f"| S: {download.aria_download().num_seeders}"
+                    msg += f"\n<b> P:</b> {download.aria_download().connections} " \
+                           f"| <b>S:</b> {download.aria_download().num_seeders}"
             msg += "\n\n"
         return msg
 
