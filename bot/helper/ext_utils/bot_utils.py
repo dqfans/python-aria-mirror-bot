e2c2d9ec16d8b7d4f91f8c791843deb9b5faddfe
noupus <63485872+noupus@users.noreply.github.com>
2020-05-25 08:32:37 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -92,8 +92,8 @@ def get_readable_message():
             msg += f"<b>📂 Name:</b> <i>{download.name()}</i>"
             msg += f"\n<b>Status:</b> <code>{download.status()}</code>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> "
-                       f"\n<b>Size:</b> {download.size()}" \
+                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
+                       f"\n<b>Size:</b> {download.size()}" 
                        f"\n<b>Speed:</b> {download.speed()} ETA: {download.eta()}"
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
